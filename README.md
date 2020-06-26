45c95d09ea213137711d748f8a1a03f1582e2e1a
AlanD <alan.doose@gmail.com>
2020-06-25 19:56:52 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 2dgame_engine
+# assets and lib folders are excluded from this repo
+# excluded lib folder includes:
+# -glm library
+# -lua5.3-dev library
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include "game.h"
+
+int main(int argc, char* args[])
+{
+    //Game* game = new Game();
+    return 0;
+}
